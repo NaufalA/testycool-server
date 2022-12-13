0a8119984f84d2ae1c1a2eac84c8b51762f1146a
NaufalAulia <naufal7600@gmail.com>
2022-12-14 06:41:40 +0700

    Initial commit

---
+++ src/main/java/com/testycool/testycoolserver/TestycoolServerApplication.java
@@ -0,0 +1,13 @@
+package com.testycool.testycoolserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestycoolServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestycoolServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testycool/testycoolserver/TestycoolServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testycool.testycoolserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestycoolServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
